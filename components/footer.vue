b177ace98edbe2089d1a24f543e932b8f4126645
1194150512@qq.com <1194150512@qq.com>
2021-07-30 13:52:26 +0800

    更新一些sitemap文件

--- components/footer.vue
+++ components/footer.vue
@@ -6,7 +6,7 @@
       All rights reserved
     </p>
     <p>
-      <img src="https://cdn.jsdelivr.net/gh/web-Hreo/nutx_blog/assets/image/beian.png" alt="小何_前端个人博客_备案图片">
+      <img src="https://cdn.jsdelivr.net/gh/web-Hreo/nutx_blog/assets/beian.png" alt="小何_前端个人博客_备案图片">
       <a target="_blank" href="https://beian.miit.gov.cn/" title="小何_前端个人博客" rel="nofollow">赣ICP备2021006777号-1</a>
     </p>
     <p>本站总访问量为 7483 次 访客数为 3325 人</p>
